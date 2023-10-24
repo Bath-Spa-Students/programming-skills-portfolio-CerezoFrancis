0f08cdd982b2ac5ab7df25e1ac3c34fdfae1f48c
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-10-23 18:12:15 -1200

    Update exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -0,0 +1,7 @@
+#Exercise 2: Greetings
+names = ['Francis', 'Gabriel', 'Sabado', 'Cerezo']
+
+print(names[0], "is the funniest")
+print(names[1], "is the funniest")
+print(names[2], "is the funniest")
+print(names[3], "is the funniest")
