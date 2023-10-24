a6703c1d1e7957ffffaf473330ac41ff139868db
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-10-23 18:06:44 -1200

    Update exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 1.py
@@ -0,0 +1,12 @@
+#Exercise 1: Variables
+msg = "Bath Spa University welcomes students from the UAE."
+print (msg)
+
+msg = """ "Being creative isn’t just about art, music, drama… although
+       that’s part of it. It’s ingrained in the way we teach. Our ethos.
+       All our students are encouraged to be creative problem-
+       solvers  they are enterprising and innovative." """
+print (msg)
+
+name = "-Professor Sue Rigby, Vice-Chancellor"
+print (name)
