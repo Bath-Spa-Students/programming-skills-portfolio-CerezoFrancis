82eaa1d9f41f31135639ac49019cb24cd6cd7c40
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-10-23 18:14:14 -1200

    Update exercise 5.py

--- Chapter 3- Data Structures/Exercises/exercise 5.py
+++ Chapter 3- Data Structures/Exercises/exercise 5.py
@@ -1,5 +1,5 @@
 #Exercise 5: Change Guest List
-guest_list = ['To: Tupac', 'To: Eminem', 'To: Kieth Powers', 'To: Tacko Fall']
+guest_list = ['To Tupac', 'To Eminem', 'To Kieth Powers', 'To Tacko Fall']
 
 Not_available_guest = 'To: Tacko Fall'
 print(f"Unfortunately, {Not_available_guest} could not make it to the dinner event.")
@@ -9,6 +9,6 @@ guest_list[guest_list.index(Not_available_guest)] = Guest_replacement
 
 print ("Updated Guest List.")
 for guest in guest_list:
-    print (f"Dear {guest},\n \n Our team would love to formally invite you in this dinner event,\t"
+    print (f"Dear {guest},\n \n Our team would love to formally invite you to this dinner event,\t"
     "\n together with other celebrities like you. Thank you.\n"
     "\n From: Francis Cerezo\n \n")
