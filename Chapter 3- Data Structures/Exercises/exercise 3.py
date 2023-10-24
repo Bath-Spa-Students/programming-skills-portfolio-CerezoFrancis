65e4f3705fd7270cfde8c682c6201ff6da2c4d9b
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-10-23 18:12:51 -1200

    Update exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -0,0 +1,7 @@
+#Exercise 3: Your Own List
+mode_of_transport = ['Honda', 'Nissan', 'Dodge', 'BMW']
+
+print(mode_of_transport[0], "is my dad's old car")
+print(mode_of_transport[1], "is a good car")
+print(mode_of_transport[2], "is my favourite car")
+print(mode_of_transport[3], "is the luxury car i want")
