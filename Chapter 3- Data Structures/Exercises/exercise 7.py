6e66c61bdd7eeb5e5f81d05c2aace9eb8eb237d3
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-10-23 18:15:28 -1200

    Update exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -0,0 +1,24 @@
+#Exercise 7: Seeing the World
+places_to_visit = ['Texas', 'Maldives', 'Japan', 'Hawaii', 'Canada']
+
+print("Original order:", places_to_visit)
+
+print("Sorted order:", sorted(places_to_visit))
+
+print("Original order:", places_to_visit)
+
+print("Reverse sorted order:", sorted(places_to_visit, reverse=True))
+
+print("Original order:", places_to_visit)
+
+places_to_visit.reverse()
+print("Reversed order:", places_to_visit)
+
+places_to_visit.reverse()
+print("Original order:", places_to_visit)
+
+places_to_visit.sort()
+print("Sorted order:", places_to_visit)
+
+places_to_visit.sort(reverse=True)
+print("Reverese sorted order:", places_to_visit)
