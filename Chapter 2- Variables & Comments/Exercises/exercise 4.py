0cab2d2e3961d3bf9bb1848d89b7cdbbded6ac0b
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:32:52 -1200

    Update exercise 4.py

--- Chapter 2- Variables & Comments/Exercises/exercise 4.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 4.py
@@ -1,8 +1,14 @@
 #Exercise 4: Favorite Number 
+#Assigning the integer 15 to the variable 'fave_num' 
 fav_num = 15
+
+#Creating a formatted string that includes the value of the variable 'fave_num' 
 msg = f"My favorite number is {fav_num}."
 print (msg)
 
+#Reassigning the variable 'fave_num' to the integer 5 
 fav_num = 5
+
+#Creating a new formatted string that includees the updated value of the variable 'fave_num'
 msg = f"My second favorite number is {fav_num}."
 print (msg)
