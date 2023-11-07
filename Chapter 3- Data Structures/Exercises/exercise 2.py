f4955ae5f9fb1772bf95875c89cbb6030404bf03
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:34:32 -1200

    Update exercise 2.py

--- Chapter 3- Data Structures/Exercises/exercise 2.py
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -1,7 +1,15 @@
 #Exercise 2: Greetings
+#Using variable 'name' to assign and create list of names
 names = ['Francis', 'Gabriel', 'Sabado', 'Cerezo']
 
+#Printing the first element of the list concatenated with the string "is the funniest"
 print(names[0], "is the funniest")
+
+#Printing the second element of the list concatenated with the string "is the funniest"
 print(names[1], "is the funniest")
+
+#Printing the third element of the list concatenated with the string "is the funniest"
 print(names[2], "is the funniest")
+
+#Printing the fourth element of the list concatenated with the string "is the funniest"
 print(names[3], "is the funniest")
