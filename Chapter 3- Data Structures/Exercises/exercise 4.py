46df0e641c8c86d801d9a3932169d80065007021
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:35:41 -1200

    Update exercise 4.py

--- Chapter 3- Data Structures/Exercises/exercise 4.py
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -1,18 +1,23 @@
 #Exercise 4: Guest List
-guest_list = ['To Tupac', 'To Eminem', 'To Kieth Powers', 'To Tacko Fall']
+#Creating a list of guest names and assigning it to the variable 'guest_list'
+guest_list = ['To: Tupac', 'To: Eminem', 'To: Kieth Powers', 'To: Tacko Fall']
 
-print(guest_list[0], "\n \n Our team would love to formally invite you to this dinner event,\t"
+#Printing the first element of the list concatenated with a long string
+print(guest_list[0], "\n \n Our team would love to formally invite you in this dinner event,\t"
     "\n together with other celebrities like you. Thank you.\n"
     "\n From: Francis Cerezo\n \n")
 
-print(guest_list[1], "\n \n Our team would love to formally invite you to this dinner event,\t"
+#Printing the second element of the list concatenated with a long string
+print(guest_list[1], "\n \n Our team would love to formally invite you in this dinner event,\t"
     "\n together with other celebrities like you. Thank you.\n"
     "\n From: Francis Cerezo\n \n")
 
-print(guest_list[2], "\n \n Our team would love to formally invite you to this dinner event,\t"
+#Printing the third element of the list concatenated with a long string
+print(guest_list[2], "\n \n Our team would love to formally invite you in this dinner event,\t"
     "\n together with other celebrities like you. Thank you.\n"
     "\n From: Francis Cerezo\n \n")
 
-print(guest_list[3], "\n \n Our team would love to formally invite you to this dinner event,\t"
+#Printing the fourth element of the list concatenated with a long string
+print(guest_list[3], "\n \n Our team would love to formally invite you in this dinner event,\t"
     "\n together with other celebrities like you. Thank you.\n"
     "\n From: Francis Cerezo\n \n")
