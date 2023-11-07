ad64a63cddcc74d41c9f36418173691f1ff7fc9c
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 17:49:01 -1200

    Update exercise 2.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 2.py
@@ -1,6 +1,15 @@
 #Exercise 2: Print the Version of Python
+#Importing sys to be able to access the system-specifics
 import sys
+
+#Labeling sys version by printing the message "Python version"
 print ("Python version")
+
+#Using the sys.version command to print the python version
 print (sys.version)
+
+#Labeling sys version information by printing the message "Version info"
 print ("Version info.")
+
+#Using the sys.version_info command to print the python version's information
 print (sys. version_info)
