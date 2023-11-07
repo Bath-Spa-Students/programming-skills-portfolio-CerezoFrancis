96c83c28b966227b9382c9210b7214c150798240
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 17:50:29 -1200

    Update exercise 5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 5.py
@@ -1,5 +1,13 @@
 #Exercise 5: Compute area of Circle
+#Assigning the value of pi to the variable
 π = 3.14  
+
+#Taking user input for the radius of the circle and converting it to a float
 Radius = float (input ("Please enter the radius of the given circle:"))  
-area_of_the_circle = π * Radius * Radius  
-print (" The area of the given circle is: ", area_of_the_circle)  
+
+#Calculating the area of the circle using the formula π * Radius * Radius
+#and storing it in the variable 'area_of_the_circle'
+area_of_the_circle = π * Radius * Radius 
+
+#Printing the result
+print (" The area of the given circle is: ", area_of_the_circle)
