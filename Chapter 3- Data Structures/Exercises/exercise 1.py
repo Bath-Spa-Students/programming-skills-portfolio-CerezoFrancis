e43aa787cba96c1f19e556ad165ddae548d601cd
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:34:02 -1200

    Update exercise 1.py

--- Chapter 3- Data Structures/Exercises/exercise 1.py
+++ Chapter 3- Data Structures/Exercises/exercise 1.py
@@ -1,7 +1,15 @@
 #Exercise 1: Names
+#Using variable 'name' to assign and create list of names
 names = ['Francis', 'Gabriel', 'Sabado', 'Cerezo']
 
+#Printing the first element of the list using the index 0
 print(names[0])
+
+#Printing the second element of the list using the index 1
 print(names[1])
+
+#Printing the third element of the list using the index 2
 print(names[2])
+
+#Printing the fourth element of the list using the index 3
 print(names[3])
