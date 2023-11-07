1a522203dda6fba29beef86932a5b4dde4eb95e4
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 17:48:09 -1200

    Update exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py
@@ -1,7 +1,11 @@
 #Exercise 1: Print Strings 
+#The first 4 lines of the rhyme in printed strings
 print ("""Twinkle, twinkle, little star,
     How I wonder what you are! 
 	 Up above the world so high,
-	 Like a diamond in the sky.
-Twinkle, twinkle, little star,
+	 Like a diamond in the sky.""")
+
+##The next 2 lines in the second stanza
+# lines of the rhyme in printed strings
+print ("""Twinkle, twinkle, little star,
     How I wonder what you are""")
