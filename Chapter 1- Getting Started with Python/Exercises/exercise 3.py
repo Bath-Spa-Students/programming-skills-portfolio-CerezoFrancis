b8054217b65dfce71aadc5c4efa24e8b63a24bdc
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 17:49:29 -1200

    Update exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 3.py
@@ -1,5 +1,12 @@
 #Exercise 3: Print date and Time
+#Manipulating the dates and times by importing datetime
 import datetime
+
+#Using the datetime.now() method to store the current date and time in the 'now' variable
 now = datetime.datetime.now()
-print ("Current date and time : ")
+
+#Printing the message "Current date and time :"
+print("Current date and time : ")
+
+#Using the the specified format of the strftime() method and printing the current date and time
 print (now. strftime("%Y-%m-%d %H:%M:%S"))
