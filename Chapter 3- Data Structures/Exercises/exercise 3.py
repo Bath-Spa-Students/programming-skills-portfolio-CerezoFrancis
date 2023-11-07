32733405d9f91f6eb8fc06d14088b3d134ac7bee
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:35:05 -1200

    Update exercise 3.py

--- Chapter 3- Data Structures/Exercises/exercise 3.py
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -1,7 +1,15 @@
 #Exercise 3: Your Own List
+#Using variable 'mode of transport' to assign and create list of mode of transports
 mode_of_transport = ['Honda', 'Nissan', 'Dodge', 'BMW']
 
+#Printing the first element of the list concatenated with the string "is my dad's old car"
 print(mode_of_transport[0], "is my dad's old car")
+
+#Printing the second element of the list concatenated with the string "is a good car"
 print(mode_of_transport[1], "is a good car")
+
+#Printing the third element of the list concatenated with the string "is my favourite car"
 print(mode_of_transport[2], "is my favourite car")
+
+#Printing the fourth element of the list concatenated with the string "is the luxury car I want"
 print(mode_of_transport[3], "is the luxury car i want")
