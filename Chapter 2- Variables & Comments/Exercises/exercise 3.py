5f2ed725c788afd5a3ce63462b890129acdb142b
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:32:28 -1200

    Update exercise 3.py

--- Chapter 2- Variables & Comments/Exercises/exercise 3.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 3.py
@@ -1,13 +1,19 @@
 #Exercise 3: Stripping Names
+#Storing the strring with the leading and trailing whitespacr characters in the variable 'name' 
 name = "\t Francis Cerezo\n"
+
+#Printing the original string
 print ("Unmodified:")
 print (name)
 
+#Using the lstrip() method to remove the leading whitespace characters
 print ("\nUsing lstrip():")
 print (name.lstrip())
 
+#Using the rstrip() method to remove the trailing whitespace characters
 print ("\nUsing rstrip():")
 print (name.lstrip())
 
+#Using the lstrip() method to remove both the leading and trailing whitespace characters
 print ("\nUsing strip():")
 print (name.strip())
