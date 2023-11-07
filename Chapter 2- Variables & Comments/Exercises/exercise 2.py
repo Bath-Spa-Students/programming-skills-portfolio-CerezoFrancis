638be6c3d084732c3ad7f84cbc2b75e753a71e23
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:31:56 -1200

    Update exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/exercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -1,5 +1,8 @@
 #Exercise 2: Variables
+#Storing the quote in a multi-line string and assigning it to the variable 'msg'
 msg = """Queen Elizabeth II said "When life seems hard, the courageous 
 do not lie down and accept defeat; instead, they are all
 the more determined to struggle for a better future.” """
+
+#Printing the contents of the variable 'msg'
 print (msg)
