10d01a5b15d1ccd79c7ca304fcdfeac39c107127
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 18:31:29 -1200

    Update exercise 1.py

--- Chapter 2- Variables & Comments/Exercises/exercise 1.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 1.py
@@ -1,12 +1,15 @@
 #Exercise 1: Variables
+#Printing the string stored in the variable 'msg'
 msg = "Bath Spa University welcomes students from the UAE."
 print (msg)
 
+#Printing the multi-line string stored in the variable 'msg'
 msg = """ "Being creative isn’t just about art, music, drama… although
        that’s part of it. It’s ingrained in the way we teach. Our ethos.
        All our students are encouraged to be creative problem-
        solvers  they are enterprising and innovative." """
 print (msg)
 
+#Printing the stored string in the variable 'name'
 name = "-Professor Sue Rigby, Vice-Chancellor"
 print (name)
