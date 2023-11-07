839c12c428d827025052c658b4a37c27aaab8681
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-06 17:49:58 -1200

    Update exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -1,7 +1,18 @@
-#Exercise 4: Strings Concatination 
+#Exercise 4: Strings Concatenation 
+#Assigning the string as "Francis+" to variable a
 a = "Francis+"
-b = "loves+"
+
+#Assigning the string as "loves+" to variable b
+b = "Loves+"
+
+#Assigning the string as "Burgers=" to variable c
 c = "Burgers="
+
+#Assigning the string as "Unhealthy" to variable d
 d = "Unhealthy"
+
+#Using the 'e' variable to store concatenating strings 'a', 'b', 'c', and 'd'
 e = a + b + c + d
+
+#Printing the concatenated string 
 print (e)
