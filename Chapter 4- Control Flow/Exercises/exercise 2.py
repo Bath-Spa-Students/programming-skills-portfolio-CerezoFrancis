a32a2494e57924b39a8c8e924ee9ec8f0bf469ad
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 20:43:38 -1200

    Update exercise 2.py

--- Chapter 4- Control Flow/Exercises/exercise 2.py
+++ Chapter 4- Control Flow/Exercises/exercise 2.py
@@ -1,1 +1,25 @@
+#Exercise 2: Alien Colors
+#The chosen color of the alien is green
+alien_color = 'green'
+#If-else chain to check the alien's color
+if alien_color == 'green':
+    
+    #If the alien's color is green, it will print the message for gaining 5 points
+    print("Well done! You have now gained 5 points for shooting the alien.")
+else:
 
+    #If the alien's color is not green, it will print the message for gaining 10 points
+    print("Well done! You have now gained 10 points.")
+
+#If the chosen clor of the alien is red (or any other color than green)
+alien_color = 'blue'
+
+#If-else chain to check the alien's color
+if alien_color == 'green':
+    
+    #If the alien's color is green, it will print the message for gaining 5 points
+    print("Well done! You have now gained 5 points for shooting the alien.")
+else:
+
+    #If the alien's color is not green, it will print the message for gaining 10 points
+    print("Well done! You have now gained 10 points.")
