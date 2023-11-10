7e08452c62cfcaf00b788adfc3b9b7535274a95d
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 21:01:05 -1200

    Update exercise 3.py

--- Chapter 6- Loops/Exercises/exercise 3.py
+++ Chapter 6- Loops/Exercises/exercise 3.py
@@ -1,1 +1,9 @@
+#Exercise 3: Infinity
+import time
 
+#A python command that will create an infinite loop that prints a message
+while True:
+    #The specified message that will go on a infinite loop
+    print("We love coding, code lab no. 1.")
+    #This command will create a pause between lines to slow down the loop
+    time.sleep(1)
