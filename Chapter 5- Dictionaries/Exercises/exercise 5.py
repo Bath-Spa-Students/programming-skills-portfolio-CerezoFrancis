1e5412d06a11b278ac0d8f1805609af73b944306
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 20:57:17 -1200

    Update exercise 5.py

--- Chapter 5- Dictionaries/Exercises/exercise 5.py
+++ Chapter 5- Dictionaries/Exercises/exercise 5.py
@@ -1,1 +1,13 @@
+#Exercise 5: Pets
+#Creating a dictionary with different pets and their owners
+pet1 = {'animal': 'hamster', 'owner': 'Brian'}
+pet2 = {'animal': 'cat', 'owner': 'Naomi'}
+pet3 = {'animal': 'parrot', 'owner': 'Logan'}
+pet4 = {'animal': 'dog', 'owner': 'Brandon'}
 
+#Store the dictionaries in a list called pets
+pets = [pet1, pet2, pet3, pet4]
+
+#looping through the list and printing everything known about each pet
+for pet in pets:
+    print(f"This pet {pet['animal']} would fit perfectly with {pet['owner']}.\n")
