7f886bc4356eaa338d8f5b54179eef972993518a
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 20:51:16 -1200

    Update exercise 5.py

--- Chapter 4- Control Flow/Exercises/exercise 5.py
+++ Chapter 4- Control Flow/Exercises/exercise 5.py
@@ -1,1 +1,34 @@
+#Exercise 5: Favorite Fruit
+#Making a list of my favorite fruits
+favorite_fruits_list = ['apple', 'orange', 'banana', 'pineapple']
 
+#Writing independent if statements to check for certain fruits in my list 
+if 'apple' in favorite_fruits_list:
+    
+    #If apple is in the list, the message will be printed
+    print("I love apple, its my number one favorite fruit!")
+
+if 'orange' in favorite_fruits_list:
+    
+    #If orange is in the list, the message will be printed
+    print("I love orange, its my second favorite fruit!")
+
+if 'banana' in favorite_fruits_list:
+    
+    #If banana is in the list, the message will be printed
+    print("I love banana, its my third favorite fruit!")
+
+if 'pineapple' in favorite_fruits_list:
+    
+    #If pineapple is in the list, the message will be printed
+    print("I love pineapple, its my fourth favorite fruit!")
+
+if 'pomagranate' in favorite_fruits_list:
+    
+    #If pomagranate is in the list, the message will be printed
+    print("I love pomagranate, its my fifth favorite fruit!")
+
+if 'peach' in favorite_fruits_list:
+    
+    #If peach is in the list, the message will be printed
+    print("I love peach, its my sixth favorite fruit!")
