6bd37321d5d4631ea4ccd9dc13006eadab8be862
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 21:02:32 -1200

    Update exercise 1.py

--- Chapter 7- Functions/Exercises/exercise 1.py
+++ Chapter 7- Functions/Exercises/exercise 1.py
@@ -1,1 +1,7 @@
+#Exercise 1: Message
+#Define a function called display_message
+def display_message():
+    print("In this chapter, we are learning about funtion in Python. Funtion is a block of code which only runs when it is called.")
 
+#Calling the display_message function
+display_message()
