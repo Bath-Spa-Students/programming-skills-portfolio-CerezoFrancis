870a89f664463ea8a1b43a17d218e61ddcb551eb
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 20:56:52 -1200

    Update exercise 4.py

--- Chapter 5- Dictionaries/Exercises/exercise 4.py
+++ Chapter 5- Dictionaries/Exercises/exercise 4.py
@@ -1,1 +1,19 @@
+#Exercise 4: Rivers
+#Creating a dictionary of the major rivers and the countries they run through
+major_rivers = {'nile': 'egypt',
+                'amazon': 'brazil',
+                'Mississippi-Missouri-Red Rock': 'U.S. states'}
 
+#Creating a loop and adding sentence to each one
+for river, country in major_rivers.items():
+    print(f"The {river.title()} river is one of the longest river and is located in {country.title()}.\n")
+
+#Printin the names of each river in my dictionary
+print("\n Names of the rivers:")
+for river in major_rivers:
+    print(river.title())
+
+#Printing the names of each country included in the dictionary
+print("\n Names of the countries:")
+for country in major_rivers.values():
+    print(country.title())
