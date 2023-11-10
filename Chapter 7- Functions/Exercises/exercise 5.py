932486bed0427bbac178329a7de5a7dd3a91cd5b
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 21:04:12 -1200

    Update exercise 5.py

--- Chapter 7- Functions/Exercises/exercise 5.py
+++ Chapter 7- Functions/Exercises/exercise 5.py
@@ -1,1 +1,11 @@
+#Exercise 5: Cities
+#Defining the describe_city function with a default country
+def describe_city(city, country='Unknown'):
+    print(f"{city} is in {country}.")
 
+#Calling the function for three different cities
+describe_city("Italy", "Southern Europe")
+describe_city("Dubai", "United Arab Emirates")
+describe_city("Texas", "United State of America")
+#Using a default country
+describe_city("California")
