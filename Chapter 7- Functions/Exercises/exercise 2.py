858af42f7a4b5ea4c5c6db4b4bbeeaafbb4b3a83
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 21:02:56 -1200

    Update exercise 2.py

--- Chapter 7- Functions/Exercises/exercise 2.py
+++ Chapter 7- Functions/Exercises/exercise 2.py
@@ -1,1 +1,7 @@
+#Exercise 2: Favorite Book
+#Defining the favorite_book function
+def favorite_book(title):
+    print(f"This book has made a really big impact in my life and the book is called {title}.")
 
+#Calling th favorite_book function with a book title as an argument
+favorite_book("RICH DAD, POOR DAD")
