0b27c04c7435a00b1da7f3df362309c82f9f2d44
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 21:03:19 -1200

    Update exercise 3.py

--- Chapter 7- Functions/Exercises/exercise 3.py
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -1,1 +1,10 @@
+#Exercise 3: T-Shirt
+#Defining the make_shirt function
+def make_shirt(size, message):
+    print(f"The size of this shirt is {size} with a print on the shirt that says: {message}")
 
+#Calling the function using positional arguments
+make_shirt("medium", "I love Python")
+
+#Calling the function using keyword arguments
+make_shirt(size="large", message="In coding we trust")
