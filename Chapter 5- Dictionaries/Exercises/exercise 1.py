6d351f7e117452c184dec802003066475e093e8a
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 20:55:29 -1200

    Update exercise 1.py

--- Chapter 5- Dictionaries/Exercises/exercise 1.py
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -1,1 +1,19 @@
+#Exercise 1: Person
+#Creating a dictionary to store the information about the person i know
+known_person = {'first_name': 'Ron',
+                'last_name': 'Cerezo',
+                'age': 20,
+                'city': 'manila'}
 
+#Printing each piece of information that I stored in my dictionary
+#Printing 'first_name' and labeling it base on what it is
+print(f"First Name: {known_person['first_name']}")
+
+#Printing 'last_name' and labeling it base on what it is
+print(f"last Name: {known_person['last_name']}")
+
+#Printing 'age' and labeling it base on what it is
+print(f"age: {known_person['age']}")
+
+#Printing 'city' and labeling it base on what it is
+print(f"city: {known_person['city']}")
