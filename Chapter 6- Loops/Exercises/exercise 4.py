8f082297daf9ec4e7c44d57e728c126af618e22c
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 21:01:27 -1200

    Update exercise 4.py

--- Chapter 6- Loops/Exercises/exercise 4.py
+++ Chapter 6- Loops/Exercises/exercise 4.py
@@ -1,1 +1,20 @@
+#Exercise 4: Deli
+#A list of various sandwiches
+sandwich_orders = ['grilled cheese', 'club', 'submarine', 'wrap', 'ham and cheese']
+#Empty the list to store the finished sandwiches
+finished_sandwiches = []
 
+#Looping through the list of sandwiches orders
+while sandwich_orders:
+    #Retrieve and remove the first sandwich order
+    current_sandwich = sandwich_orders.pop(0)
+    #For all the sandwich that has been made this message will be printed
+    print(f"Ma'am/Sir your delightful {current_sandwich} sandwich has been made. We hope you like it!")
+    #with every finished sandwich it will be added to the list of finished sandwiches
+    finished_sandwiches.append(current_sandwich)
+
+#A list of the finished sandwiches
+print("\nSandwiches that have been made:")
+for sandwich in finished_sandwiches:
+    #Printing each finished sandwich with the fist letter capitalized
+    print(sandwich.capitalize())
