ecb79179457a61207cc7de7a56c0927461d55977
CerezoFrancis <144208176+CerezoFrancis@users.noreply.github.com>
2023-11-09 20:55:59 -1200

    Update exercise 2.py

--- Chapter 5- Dictionaries/Exercises/exercise 2.py
+++ Chapter 5- Dictionaries/Exercises/exercise 2.py
@@ -1,1 +1,16 @@
+#Exercise 2: Glossary
+#Creating a glossary with words that I have learned from previous chapters 
+glossary =  {
+    'variable': 'A named reference to a value that can be changed.',
+    'string': 'A collection of alphabets, words orother characters.',
+    'function': 'A block of organized, reusable code that is used to perform a single, related action.',
+    'import': 'To make a code in one module available in another.',
+    'concatenation': 'Joining two or more strings to create a single new string.'
+}
 
+#Printing each word and its meaning
+print(f"Variable: \t{glossary['variable']}\n")
+print(f"String: \t{glossary['string']}\n")
+print(f"Function: \t{glossary['function']}\n")
+print(f"Import: \t{glossary['import']}\n")
+print(f"Concatenation: \t{glossary['concatenation']}\n")
